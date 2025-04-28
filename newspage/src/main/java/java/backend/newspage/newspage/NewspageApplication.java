fbb946fa7e68a1b4205b5edc49a3ef30bf966d09
chihiroj <77557825+chihiroj@users.noreply.github.com>
2025-04-28 21:53:46 +0200

    Initial commit

---
+++ newspage/src/main/java/java/backend/newspage/newspage/NewspageApplication.java
@@ -0,0 +1,13 @@
+package java.backend.newspage.newspage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewspageApplication.class, args);
+	}
+
+}
---
+++ newspage/src/test/java/java/backend/newspage/newspage/NewspageApplicationTests.java
@@ -0,0 +1,13 @@
+package java.backend.newspage.newspage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
